1547f0ee3543509fd18a563ce31efbd2f532e859
Brian-Cox-22 <b002cbsc.bsc@gmail.com>
2025-12-04 23:01:33 -0800

    Created player, circleshape classes. Added 'triangular' player to the screen'

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
+    # initialize screen object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # initialize player object
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     
+    # runing checks, prints version, screen size
     version = pygame.version.ver
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # new clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # game loop, runs forever right now
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # something about .fill
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = (clock.tick(60))/1000
+        # print(dt)
     
     
 
